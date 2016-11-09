891fa2054b663aa50ed0240d18bff17cddb5dc88
GerganaDimitrova <dimitrovag97@abv.bg>
2016-11-09 23:40:38 +0200

    Add files via upload

---
+++ task04.cpp
@@ -0,0 +1,34 @@
+// ConsoleApplication18.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num, n;
+	cin >> n;
+	int numMax = INT_MIN;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Insert number: ";
+		cin >> num;
+		if (num < 0 && num > numMax) 
+		{
+			numMax = num;
+		}
+	}
+	if (numMax == INT_MIN)
+	{
+		cout << "You have to insert a negative number." << endl;
+	}
+	else
+	{
+		cout << numMax;
+	}
+    return 0;
+}
+
